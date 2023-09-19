68d7e068b3401309225c083cf97fb18ac2d1ad0c
GenaPlem <shainkill1337@gmail.com>
2023-09-19 19:21:30 +0100

    Add class Player

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+class Player:
+    def __init__(self, username, attack_dmg, heath_points, coins):
+        self.username = username
+        self.attack_dmg = attack_dmg
+        self.heath_points = heath_points
+        self.coins = coins
