5e993b774175ba90610c23d7cd259a26e5b689a9
GenaPlem <shainkill1337@gmail.com>
2023-09-19 19:24:13 +0100

    Add initialize game function

--- run.py
+++ run.py
@@ -4,3 +4,12 @@ class Player:
         self.attack_dmg = attack_dmg
         self.heath_points = heath_points
         self.coins = coins
+
+
+def initialize_game():
+    username = input('Enter your Name! ')
+    new_user = Player(username, 10, 100, 0)
+    print(f" Hey, {new_user.username}. You finally awake! Hope you're fine.")
+
+
+initialize_game()
