8251d874f1e6877d20a59ba019c8fac1f7444750
GenaPlem <shainkill1337@gmail.com>
2023-09-21 13:57:06 +0100

    Add helper function to continue the game after some action

--- run.py
+++ run.py
@@ -30,7 +30,7 @@ class Enemy:
 
 def clear():
     """
-    Function to clear the terminal
+    Helper function to clear the terminal
     """
     os.system('clear')
 
@@ -59,7 +59,17 @@ def text_align_center(text):
     print(text.center(shutil.get_terminal_size()[0]))
 
 
+def continue_input():
+    """
+    Helper function to continue the game
+    """
+    input("> Press 'Enter' to continue")
+
+
 def show_stats(player):
+    """
+    Function to show stats of player
+    """
     clear()
     print('*' * 30)
     print(f'LOCATION: {player.location}')
@@ -76,7 +86,6 @@ def initialize_game():
     Function to initialize the game after creating a new player
     """
     while True:
-        # clear()
         ascii_art_logo()
         username = input('Enter your name Hero! \n')
         if len(username.strip()) > 1:
@@ -84,28 +93,25 @@ def initialize_game():
             return player
         else:
             print('Name should be more than 1 symbol')
-            input("> Press 'Enter' to continue")
+            continue_input()
 
 
 def start_new_game(player):
     """
     Function for game intoduction
     """
-    # clear()
     show_stats(player)
     print('You wake up in a dimly lit cave, the flickering light of a campfire casting shadows on the walls.')
     print('A Stranger sits across from you, tending to the fire.\n')
-    input("> Press 'Enter' to continue")
-    # clear()
+    continue_input()
     show_stats(player)
     print(f'''Stranger:
         Hey {player.username}, you're finally awake.
         I heard a dragon's roar and then found you unconscious at the foot of the mountain.
         You must have been at the summit where, according to legends, a fearsome Dragon resides.
     ''')
-    input("> Press 'Enter' to continue")
-    # print(f'Your health is {player.hp}/{player.max_hp}HP')
-    # print('and the stranger offers you a mysterious potion.\n')
+    continue_input()
+    show_stats(player)
     print(f'''Stranger:
         This will help you recover
         *He offers you a mysterious potion*
@@ -113,23 +119,21 @@ def start_new_game(player):
     ''')
     drink_potion = input('# ').lower()
     if drink_potion == 'y':
-        # clear()
         player.hp = player.max_hp
-        # print('You drink that potion')
         show_stats(player)
         print(f'Your health now is {player.hp}/{player.max_hp}HP\n')
         print('''Stranger:
         Good. Now you are ready to go.
         One more thing. If you want to get more potions like this, just find the merchant in the village
         ''')
-        input("> Press 'Enter' to continue")
+        continue_input()
     elif drink_potion == 'n':
         show_stats(player)
         print('''Stranger:
         As you say so...
         By the way, if you are interested in survive, you have to find the merchant in the village
         ''')
-        input("> Press 'Enter' to continue")
+        continue_input()
 
 
 def cave_actions(player):
@@ -150,12 +154,12 @@ def cave_actions(player):
         Ah, you're still here? Time waits for no one, especially not in these treacherous lands. I suggest you move along.
         I have my own matters to attend to.
         ''')
-        input("> Press 'Enter' to continue")
+        continue_input()
         cave_actions(player)
     else:
         print("No such options. Choose it from the options list.")
         print("Press 'Enter' to continue")
-        input("> Press 'Enter' to continue")
+        continue_input()
         cave_actions(player)
 
 
@@ -163,15 +167,13 @@ def show_rules():
     """
     Function to show rules of the game
     """
-    # clear()
     ascii_art_logo()
     print("------------------------------------------------")
     print("1. You will be presented with a list of options at each stage of the game.")
     print("2. To make a choice, simply enter the number corresponding to the option you'd like to choose.")
     print("3. To go to main menu and save the game at any time, enter '0'.")
     print("------------------------------------------------")
-    text_align_center("Press 'Enter' to return to the main menu.")
-    input('> ')
+    continue_input()
     main_menu()
 
 
@@ -201,7 +203,7 @@ def main_menu():
         quit()
     else:
         print('No such options! Please select number from menu options. Press "Enter" to continue')
-        input('> ')
+        continue_input()
         main_menu()
 
 
